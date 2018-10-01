2049933548846b023262ec0cae1d1e65dd14b254
于子軒 <geniustanley@gmail.com>
2018-10-01 14:27:49 +0800

    update

--- boot.js
+++ boot.js
@@ -10,7 +10,7 @@ if (!window.location.hash) {
         function($0, $1, $2, $3) { fragmentQueryParameters[$1] = $3; }
     );
 
-    fitbitAccessToken = fragmentQueryParameters.access_token;
+    /*fitbitAccessToken = fragmentQueryParameters.access_token;*/
     fitbitAccessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2V0dDRkMiLCJhdWQiOiIyMjdHNUwiLCJpc3MiOiJGaXRiaXQiLCJ0eXAiOiJhY2Nlc3NfdG9rZW4iLCJzY29wZXMiOiJ3aHIgd251dCB3cHJvIHdzbGUgd3dlaSB3c29jIHdzZXQgd2FjdCB3bG9jIiwiZXhwIjoxNTM4NDYwMDE1LCJpYXQiOjE1MzgzNzM2MTV9.iv5Hmg1_2WZWYxpFy1bWcu2hh2hORbIlCdlSnIKvZfo';
     console.log(fitbitAccessToken);
 }
